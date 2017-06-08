e056c0690803bd04fcae41b4d63f7b826011c1b9
Florent-Olivon <florent.olivon@cnrs.fr>
2017-06-08 11:27:57 +0200

    Add files via upload

---
+++ bootstrap.py
@@ -0,0 +1,27 @@
+###################################################
+############# bootstrap.py ########################
+###################################################
+
+# This file contains the inforamation to access the
+# MySQL database. This file should be personal and
+# not shared with anyone.
+
+
+database_name = "default_database" # Default value
+table_name = "default_table" # Default value
+
+
+###################################
+## MacOS part
+user_account_name="root" # Value to change 
+
+mamp_ip_adress = "127.0.0.1" # Default value
+mamp_port = 8889 # Default value
+
+
+###################################
+## Windows part
+host="localhost" # Default value
+port = 3306 # Default value
+user="root"
+
